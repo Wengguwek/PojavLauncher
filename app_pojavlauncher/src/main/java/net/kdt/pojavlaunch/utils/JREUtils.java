12b63d314c4c6c9b3cfc6d5677aac746e3bb11cd
Wengguwek <99054112+Wengguwek@users.noreply.github.com>
2023-09-05 14:09:43 +0800

    Update JREUtils.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -448,7 +448,7 @@ public class JREUtils {
             case "opengles2_5":
             case "opengles3":
                 renderLibrary = "libgl4es_114.so"; break;
-            case "opengles3": renderLibrary = "libvgpu.so"; break;
+            case "opengles3_vgpu": renderLibrary = "libvgpu.so"; break;
             case "opengles3_virgl":
             case "vulkan_zink": renderLibrary = "libOSMesa_8.so"; break;
             case "opengles3_desktopgl_angle_vulkan" : renderLibrary = "libtinywrapper.so"; break;
